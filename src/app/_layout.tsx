e9d4f61c6ef6884d69c807843a75ce8482413716
Denian Camilotti Fossatti <denian_fossatti@hotmail.com>
2025-08-30 00:35:29 -0300

    feat: initial dependencies configuration and tailwind + RNR setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,12 @@
 import "../global.css";
 import { Slot } from "expo-router";
+import { PortalHost } from "@rn-primitives/portal";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <>
+      <Slot />
+      <PortalHost />
+    </>
+  );
 }
